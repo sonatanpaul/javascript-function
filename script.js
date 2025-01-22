d057d1d39bfea4b4bbcf229c18b3295f51b587b9
sonatanpaul <iamsonatanpaul@gmail.com>
2025-01-22 21:23:32 +0600

    learn javascript function with default parametar

--- script.js
+++ script.js
@@ -11,10 +11,23 @@ const printMy = function () {
 };
 // printMy();
 
-
 // function as a parameter and argument
 function printThis(param1, param2) {
   console.log("Hello Word", param1, param2);
 }
 // printThis("Sonatan", "Paul");
 
+// function with return keyword
+function sum(a, b) {
+  let value = a + b;
+  return value;
+}
+// sum(5, 6);
+
+// function with default parameter
+function number(a, b = 10) {
+  let result = a + b;
+  console.log(result);
+  return result;
+}
+// number(8);
